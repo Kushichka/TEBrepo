187fabdfc74ce858ef34f68e135fe4aee8ca472e
Viktor <kushichka@gmail.com>
2021-02-10 13:04:07 +0100

    z27 - done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,7 +16,8 @@ public class Main {
         //z24();
         //z25();
         //z26();
-        z28();
+        z27();
+        //z28();
     }
     public static void printAreaRound() {
         Scanner scanner = new Scanner(System.in);
@@ -275,6 +276,76 @@ public class Main {
         {
             System.out.println("Nie można zbudować trójkąt");
         }
+    }
+    public static void z27(){
+        Scanner scan = new Scanner (System.in);
+        int number = scan.nextInt();
+        String romanDigit = "";
+        if (number < 0 || number > 3999) {
+            System.out.println( "Numer od 0 do 3999");
+        }
+        else
+        {
+            System.out.print(number + " = " + romanDigit);
+            System.out.print(convert(number));
+        }
+    }
+    public static String romanDigit(int n, String one, String five, String ten){
+
+        if(n >= 1)
+        {
+            if(n == 1)
+            {
+                return one;
+            }
+            else if (n == 2)
+            {
+                return one + one;
+            }
+            else if (n == 3)
+            {
+                return one + one + one;
+            }
+            else if (n==4)
+            {
+                return one + five;
+            }
+            else if (n == 5)
+            {
+                return five;
+            }
+            else if (n == 6)
+            {
+                return five + one;
+            }
+            else if (n == 7)
+            {
+                return five + one + one;
+            }
+            else if (n == 8)
+            {
+                return five + one + one + one;
+            }
+            else if (n == 9)
+            {
+                return one + ten;
+            }
+
+        }
+        return "";
+    }
+    public static String convert(int number){
+
+        String romanOnes = romanDigit( number%10, "I", "V", "X");
+        number /=10;
+        String romanTens = romanDigit( number%10, "X", "L", "C");
+        number /=10;
+        String romanHundreds = romanDigit(number%10, "C", "D", "M");
+        number /=10;
+        String romanThousands = romanDigit(number%10, "M", "", "");
+
+        String result = romanThousands + romanHundreds + romanTens + romanOnes;
+        return result;
     }
 
 }
